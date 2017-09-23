83f59e7f3314fc95aaf05014a3705b6341a3cdc2
Vinicius A <vba321@hotmail.com>
2017-09-23 08:08:06 -0300

    use fake implementation

--- cart-shopping/spec/item_spec.rb
+++ cart-shopping/spec/item_spec.rb
@@ -3,9 +3,7 @@ require_relative '../app/item'
 require_relative '../app/product'
 
 describe Item do
-  let(:product_params) { { name: 'Livro', price: 10.50 } }
-  subject(:product) { Product.new product_params }
-
+  let(:product) { instance_double('Product', name: 'Livro', price: 10.50) }
   let(:item_params) { { product: product, quantity: 3 } }
   subject(:item) { described_class.new item_params }
 
@@ -21,6 +19,7 @@ describe Item do
 
   describe "#total_value" do
     it 'item value is 31.50' do
+      allow(product).to receive(:price).and_return(10.50)
       expect(item.total_value).to eq(31.50)
     end
   end
